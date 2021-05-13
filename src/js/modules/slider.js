b88e9ad98a1a6936fded82b08b683dcb32cfcdac
Roman <roma.franko99@gmail.com>
2021-05-13 23:39:42 +0300

    Show card in 3 slide

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -15,6 +15,25 @@ export default class Slider {
         if (n < 1) {
             this.slideIndex = this.slides.length;
         }
+
+
+        try{          
+            this.hanson.style.opacity = '0';
+            if (n === 3){
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else  {
+                this.hanson.classList.add('classInUp');
+            }
+
+        } catch (e) {
+            console.log(e);
+        }
+
+
         this.slides.forEach(slide => {
             slide.style.display = 'none';           
         });
@@ -27,6 +46,11 @@ export default class Slider {
     }
 
     render(){
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch (e){
+            console.log(e);
+        }
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
                 this.plusSlides(1);
